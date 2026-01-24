344daae29931aacdbf2ed503aae8c91d7a6befb9
Shyavan Sridhar <35411334+ShyavanS@users.noreply.github.com>
2026-01-23 23:42:39 -0500

    updated for turbidity reaction with photoresistor setup

--- README.md
+++ README.md
@@ -4,8 +4,12 @@ A temporary datalogging codebase for the McMaster Chem-E Car Team Braking Sub-Te
 This project uses an Arduino UNO microcontroller programmed in the Arduino programming language. A simple setup for them to gather data for their reactions while the current car is still being created. This facilitates the creation of a linear regression model on both reactions' progression whose coefficient of determination is used to identify the reaction that the team proceeds with for the competition. All data is output via serial to Excel Data Streamer.
 
 The major components used are as follows:<br />
-1 - DS18B20 Temperature Sensor (for monitoring stopping reaction progress)<br />
-1 - SEN0189 Turbidity Sensor (for monitoring stopping reaction progress)<br />
+1 - GL5516 LDR (for monitoring stopping reaction progress)<br />
+1 - 5mm White LED (light source for LDR)<br />
+1 - 150Ω resistor (current limiting for LED)<br />
+3 - 5kΩ resistors (for constructing wheatstone bridge)<br />
 1 - Arduino UNO Microcontroller<br />
 
 The main objective of the competition is to design a small car to carry a certain amount of weight a set distance while powering and stopping the vehicle with a chemical reaction. The distance and weight vary each year. The compectition is hosted by the American Institute of Chemical Engineers (AIChE). See the corresponding repository for the current year's car codebase for more information on this year's setup.
+
+Makes use of [timonbraun02's digital low-pass filter library](https://github.com/timonbraun02/digital_filter_arduino/tree/main) to remove noise due to stir bar oscillations at a frequency of 20-25 Hz.
--- braking_datalogging_25-26_code.ino
+++ braking_datalogging_25-26_code.ino
@@ -2,88 +2,30 @@
 // Braking Datalogging Code - 25/26
 
 /*
-Some datalogging code for the Braking sub-team to run on the previous year's car.
-It uses an arduino UNO platform so that they can complete their testing of the
-reactions temporarily while the Mechanical sub-team continues to work on getting
-this year's car chassis fabricated. Two reactions are being tested, a turbidity
-varying reaction and a temperature varying reaction. All data is output via
-serial to Excel Data Streamer.
+Some datalogging code for the Braking sub-team to run on a temporary
+datalogging setup. It uses an arduino UNO platform so that they can complete
+their testing of the reactions temporarily while the Mechanical sub-team
+continues to work on getting this year's car chassis fabricated. The reaction
+is a cinnamaldehyde clock which varies in turbidity and is measured using a
+photoresistor and LED light source across from each other with a beaker filled
+with the reactants in the centre. All data is output via serial to Excel
+Data Streamer.
 */
 
 // Included libraries
-#include <OneWire.h>
-#include <DallasTemperature.h>
+#include "filter_lib.h"
 
-#define ONE_WIRE_BUS 2    // Pin for the DS18B20 data line
-#define TURBIDITY_SENS A0 // Pin for the turbidity sensor
+// Pins for the turbidity sensor
+#define TURBIDITY_SENS1 A1
+#define TURBIDITY_SENS2 A2
 
-OneWire one_wire(ONE_WIRE_BUS);            // create a OneWire instance to communicate with the sensor
-DallasTemperature temp_sensors(&one_wire); // pass oneWire reference to Dallas Temperature sensor
-
-// variables to store temperature
-double temperature_c; // Current temperature
-double init_temp;     // Initial temperature for differential calculation
-
-// Last temperature fetched flag
-bool last_fetch;
+lowpass_filter turbidity_filter(5); // low-pass filter to reduce oscillations from stir bar
 
 // Variables to store turbidity information
-double turbidity_v;
-double init_turbidity;
-
-// KALMAN FILTER variables
-double x_temp; // Filtered temperature
-double p_temp; // Initial error covariance
-
-// Process noise and measurement noise
-double q_temp; // Process noise covariance
-double r_temp; // Measurement noise covariance
-
-/*
-Description: Subroutine to implement Kalman filtering on temperature sensor data.
-Inputs:      void
-Outputs:     (double)x_temp, (double)p_temp
-Parameters:  (double)x_k, (double)p_k, (double)q, (double)r, (double)input
-Returns:     void
-*/
-void kalman_filter(double x_k, double p_k, double q, double r, double input)
-{
-  // Kalman filter prediction
-  double x_k_minus = x_k;     // Predicted next state estimate
-  double p_k_minus = p_k + q; // Predicted error covariance for the next state
-
-  // Kalman filter update
-
-  /* Kalman gain: Calculated based on the predicted error covariance
-  and the measurement noise covariance, used to update the
-  state estimate (x_k) and error covariance (p_k). */
-  double k = p_k_minus / (p_k_minus + r); // Kalman gain
-
-  // Comparison with actual sensor reading
-  x_k = x_k_minus + k * (input - x_k_minus); // Updated state estimate
-  p_k = (1 - k) * p_k_minus;                 // Updated error covariance
-
-  // Output results and update global variables
-  x_temp = x_k;
-  p_temp = p_k;
-}
-
-/*
-Description: Subroutine to fetch temperature form the temperature sensor when the data is ready.
-Inputs:      void
-Outputs:     (double)temperature_c
-Parameters:  void
-Returns:     void
-*/
-void fetch_temp(void)
-{
-  temperature_c = temp_sensors.getTempCByIndex(0); // Get temperature in Celsius
-
-  // Update temperature kalman filter
-  kalman_filter(x_temp, p_temp, q_temp, r_temp, temperature_c);
-
-  last_fetch = true; // Raise fetch flag to signal ready
-}
+double turbidity_raw;
+double turbidity_filtered;
+double turbidity_ldr;
+double turbidity_ref;
 
 /*
 Description: Arduino setup subroutine.
@@ -98,23 +40,8 @@ void setup()
 
   analogReference(EXTERNAL); // Use 3.3V reference instead of default 5V
 
-  temp_sensors.begin();                     // Initialize the DS18B20 sensor
-  temp_sensors.setResolution(11);           // Reduce resolution for faster polling
-  temp_sensors.requestTemperatures();       // Request temperature from all devices on the bus
-  temp_sensors.setWaitForConversion(false); // Disable blocking to allow multitasking
-
-  init_temp = temp_sensors.getTempCByIndex(0); // Get temperature in Celsius
-  temperature_c = init_temp;                   // Initialize temperature variable
-  last_fetch = true;                           // Raise fetch flag to signal ready
-
-  // Turbidity readings as V
-  init_turbidity = (double)analogRead(TURBIDITY_SENS) * 3.3f / 1024.0f;
-
-  // Initialize Kalman filter parameters
-  x_temp = init_temp; // Initial state estimate
-  p_temp = 0.1;       // Initial error covariance
-  q_temp = 0.01;      // Process noise covariance
-  r_temp = 0.5;       // Measurement noise covariance
+  pinMode(TURBIDITY_SENS1, INPUT);
+  pinMode(TURBIDITY_SENS2, INPUT);
 }
 
 /*
@@ -126,29 +53,27 @@ Returns:     void
 */
 void loop()
 {
-  // Only poll if flag indicates ready state
-  if (last_fetch)
+  turbidity_raw = 0.0f; // Reset turbidity reading
+
+  // Loop for averaging to reduce random noise, differential signal from wheatstone bridge
+  for (int i = 0; i < 10; i++)
   {
-    temp_sensors.requestTemperatures(); // Request temperature from all devices on the bus
+    turbidity_ldr = (double)analogRead(TURBIDITY_SENS1);
+    turbidity_ref = (double)analogRead(TURBIDITY_SENS2);
 
-    last_fetch = false; // System is no longer ready, lower flag
-  }
-  else if (temp_sensors.isConversionComplete())
-  {
-    fetch_temp(); // Fetch temperature after conversion, otherwise continue loop
+    turbidity_raw += turbidity_ldr - turbidity_ref;
   }
 
-  // Turbidity readings as V (uncalibrated output)
-  turbidity_v = (double)analogRead(TURBIDITY_SENS) * 3.3f / 1024.0f;
+  // Rescale to match range of RP2040's 12-bit ADC on final design
+  // Gain of 1.5 and DC bias of 1.65V to be implemented in differential instrumentation amp in final version
+  turbidity_raw = (turbidity_raw / 10.0f * 4095.0f / 1023.0f + 2047.0f) * 1.5f;
+  turbidity_filtered = turbidity_filter.filter(turbidity_raw);
 
-  // Output all necessary data for datalogging in CSV format for Excel Data Streamer
   Serial.print((double)micros() * 1e-6, 6);
   Serial.print(",");
-  Serial.print(temperature_c, 6);
+  Serial.print(turbidity_raw);
   Serial.print(",");
-  Serial.print(x_temp, 6);
-  Serial.print(",");
-  Serial.println(turbidity_v, 6);
+  Serial.println(turbidity_filtered);
 
-  delayMicroseconds(150000); // Delay to ensure Excel's data streamer can keep up with the data being sent
+  delay(10); // Delay for data streamer
 }
